b9cc338db2482146145c022c6c3100535fdd910c
Pedro Lacombe <pedrocarlantonio@gmail.com>
2020-06-03 00:10:35 -0300

    Added default Spring Boot Java project

---
+++ src/main/java/com/bemobi/BemobiApplication.java
@@ -0,0 +1,13 @@
+package com.bemobi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BemobiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BemobiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bemobi/BemobiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bemobi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BemobiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
