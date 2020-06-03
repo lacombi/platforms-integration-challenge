e63f0aa2115ad422cc6ae2898f730421a53b8059
Pedro Lacombe <pedrocarlantonio@gmail.com>
2020-06-03 00:45:49 -0300

    Added the getFromQueue method, to retrieve queued messages

--- src/main/java/com/bemobi/service/QueueService.java
+++ src/main/java/com/bemobi/service/QueueService.java
@@ -1,9 +1,13 @@
 package com.bemobi.service;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.amazonaws.services.sqs.AmazonSQS;
+import com.amazonaws.services.sqs.model.Message;
+import com.amazonaws.services.sqs.model.ReceiveMessageRequest;
 import com.amazonaws.services.sqs.model.SendMessageRequest;
 import com.bemobi.aws.AmazonProperty;
 import com.bemobi.model.Product;
@@ -24,6 +28,13 @@ public class QueueService {
 				.withMessageBody(JsonUtils.mapToJson(product))
 				.withDelaySeconds(5);
 		this.amazonSQS.sendMessage(messageRequest);
+	}
+	
+	public List<Message> getFromQueue() {
+		ReceiveMessageRequest messageRequest = new ReceiveMessageRequest()
+				.withQueueUrl(this.property.getSqs().getQueueUrl())
+				.withWaitTimeSeconds(5);
+		return this.amazonSQS.receiveMessage(messageRequest).getMessages();
 	}
 	
 }
