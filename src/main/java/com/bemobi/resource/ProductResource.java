be53fbb6bae993c1433d428e76fe6508e398538d
Pedro Lacombe <pedrocarlantonio@gmail.com>
2020-06-03 00:28:05 -0300

    Created ProductResource to receive HTTP Request

---
+++ src/main/java/com/bemobi/resource/ProductResource.java
@@ -0,0 +1,37 @@
+package com.bemobi.resource;
+
+import javax.validation.Valid;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.bemobi.dto.ProductDto;
+import com.bemobi.model.Product;
+import com.bemobi.service.ProductService;
+
+@RestController
+@RequestMapping("/products")
+public class ProductResource {
+
+	@Autowired
+	private ProductService productService;
+
+	private static final Logger LOG = LoggerFactory.getLogger(ProductResource.class);
+
+	@PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE)
+	public ResponseEntity<HttpStatus> product(@Valid @RequestBody ProductDto dto) {
+		LOG.info("HTTP request received:" + dto);
+		Product product = this.productService.create(dto.build());
+		LOG.info("HTTP request generated:" + product);
+		return ResponseEntity.accepted().build();
+	}
+
+}
